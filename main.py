2928b75b3f67d0d9c9dbdf46b063fb44e68f73a6
wooft <wooftyaa@gmail.com>
2022-10-06 00:46:44 +0300

    123

--- main.py
+++ main.py
@@ -1,27 +1,33 @@
 from Stack import Stack
 
-New_Stack = Stack()
-str_0 = '(((([{}]))))'
-str_1 = '}{}'
-str_2 = '[([])((([[[]]])))]{()}'
-str_3 = '{{[(])]}}'
+# str = '[([])((([[[]]])))]{()}'
+str = '(((())))'
+str_1 = '(())))'
 
-def isbalance(str: str):
-    open_stack = Stack()
-    close_stack = Stack()
-    if len(str) % 2 == 0: #Если количество элементов строки нечетное, значит последовательность несбалансированная
-        for symbol in str:
-            if symbol in ['(', '[', '{']:
-                open_stack.push(symbol)
+
+# ['[','{','(']
+# [']','}',')']
+
+def isbalance(str: str) -> str:
+    openstack = Stack()
+    closestack = Stack()
+    if len(str) % 2 == 0:
+        for element in str:
+            if element == '(':
+                openstack.push(element)
             else:
-                close_stack.push(symbol)
-        print(open_stack.allElements())
-        print(close_stack.allElements())
+                closestack.push(element)
+                if openstack.peek() == '(' and closestack.peek() == ')':
+                    openstack.pop()
+                    closestack.pop()
+        if openstack.isEmpty() == True and closestack.isEmpty() == True:
+            return 'Сбалансировано'
+        else:
+            return 'Несбалансировано'
     else:
-        return 'Несбалансированно'
+        return 'Несбалансировано'
+
 
 if __name__ == '__main__':
-    print(isbalance(str_2))
+    print(isbalance(str))
     # print(isbalance(str_1))
-    # print(isbalance(str_2))
-    # print(isbalance(str_3))
