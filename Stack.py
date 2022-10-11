f0144790a89db4a1b995ecb77974567b1cc18253
Михаил Колесниченко <wooftyaa@gmail.com>
2022-10-11 22:28:46 +0300

    Finish commit

--- Stack.py
+++ Stack.py
@@ -3,33 +3,33 @@ class Stack():
         self.i = 0
         self.list_elements = []
 
-    def __iter__(self): #Делаем класс итерируемым
+    def __iter__(self):  # Делаем класс итерируемым
         return self
 
-    def __next__(self): #Метод Next возвращает элементы в обратной последовательности
+    def __next__(self):  # Метод Next возвращает элементы в обратной последовательности
         self.i += 1
 
         if self.i > len(self.list_elements):
             raise StopIteration
         return self.list_elements[-self.i]
 
-    def isEmpty(self): #проверка стека на пустоту. Метод возвращает True или False.
+    def isEmpty(self):  # проверка стека на пустоту. Метод возвращает True или False.
         if len(self.list_elements) == 0:
             return True
         else:
             return False
 
-    def push(self, something): #добавляет новый элемент на вершину стека. Метод ничего не возвращает.
+    def push(self, something):  # добавляет новый элемент на вершину стека. Метод ничего не возвращает.
         self.list_elements.append(something)
 
-    def pop(self): #удаляет верхний элемент стека. Стек изменяется. Метод возвращает верхний элемент стека
+    def pop(self):  # удаляет верхний элемент стека. Стек изменяется. Метод возвращает верхний элемент стека
         pop = self.list_elements.pop(-1)
         return pop
 
-    def peek(self): #возвращает верхний элемент стека, но не удаляет его. Стек не меняется.
+    def peek(self):  # возвращает верхний элемент стека, но не удаляет его. Стек не меняется.
         return self.list_elements[-1]
 
-    def size(self): #возвращает количество элементов в стеке.
+    def size(self):  # возвращает количество элементов в стеке.
         return len(self.list_elements)
 
     def allElements(self):
--- main.py
+++ main.py
@@ -1,27 +1,33 @@
 from Stack import Stack
 
-New_Stack = Stack()
-str_0 = '(((([{}]))))'
-str_1 = '}{}'
-str_2 = '[([])((([[[]]])))]{()}'
+str = '(((())))'
+str_1 = '(())))'
+str_2 = '(((([{}]))))'
 str_3 = '{{[(])]}}'
 
-def isbalance(str: str):
-    open_stack = Stack()
-    close_stack = Stack()
-    if len(str) % 2 == 0: #Если количество элементов строки нечетное, значит последовательность несбалансированная
-        for symbol in str:
-            if symbol in ['(', '[', '{']:
-                open_stack.push(symbol)
-            else:
-                close_stack.push(symbol)
-        print(open_stack.allElements())
-        print(close_stack.allElements())
+def isbalance(str: str) -> str:
+    openstack = Stack()
+    closestack = Stack()
+    if len(str) % 2 == 0: #Проверка. Если количество элементов строки - нечетное, значит она несбалансирована
+        for element in str:
+            if element in ['[','{','(']: #Если элемент - открывающая скобка, добавляем её в открывающий стак
+                openstack.push(element)
+            else: #Если нет, то обрабатываем исключение
+                closestack.push(element) #Добавляем элемент во второй стак.
+                if openstack.isEmpty() == True: #Проверяем если открывающий стак пуст - возвращаем значение "Несбалансировано"
+                    return 'Несбалансировано'
+                if openstack.peek() == '(' and closestack.peek() == ')' or openstack.peek() == '[' and closestack.peek() == ']' or openstack.peek() == '{' and closestack.peek() == '}' : #Проверяем, если последние элементы стаков - парные скобки
+                    openstack.pop()
+                    closestack.pop() #удаляем оба элемента
+        if openstack.isEmpty() == True and closestack.isEmpty() == True:
+            return 'Сбалансировано'
+        else:
+            return 'Несбалансировано'
     else:
-        return 'Несбалансированно'
+        return 'Несбалансировано'
 
 if __name__ == '__main__':
+    print(isbalance(str))
+    print(isbalance(str_1))
     print(isbalance(str_2))
-    # print(isbalance(str_1))
-    # print(isbalance(str_2))
-    # print(isbalance(str_3))
+    print(isbalance(str_3))
